8a4e16ba5407391846207071b62af8943d428109
Brian Downey <bdowney3@huskers.unl.edu>
2021-08-29 00:40:36 +0000

    initial commit

---
+++ hack1.c
@@ -0,0 +1,18 @@
+ /**
+  * Author: Brian Downey
+  * Date: 2021/08/28
+  *
+  * A simple hack1 program in C
+  *
+  */
+  #include <stdlib.h>
+  #include <stdio.h>
+
+  int main(int argc, char **argv) {
+
+     printf("Brian Downey\n");
+     printf("Working to become a Civil Engineer!\n");
+
+     return 0;
+
+  }
